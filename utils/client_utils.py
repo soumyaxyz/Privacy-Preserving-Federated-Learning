ec3432495302f19eba862903ef34d367685429ac
Soumya Banerjee <soumyabanerjee@hotmail.com>
2023-07-09 00:04:47 -0400

    Lock_for_investigation

--- utils/client_utils.py
+++ utils/client_utils.py
@@ -45,6 +45,7 @@ def load_partitioned_datasets(num_clients: int, val_percent = 10, batch_size=32,
     return split_dataset(trainset, testset, num_clients, val_percent, batch_size)
 
 
+
         
 class FlowerClient(fl.client.NumPyClient):
     def __init__(self, cid, net, trainloader, valloader, N , wandb_logging=False, dataset_name='CIFAR10'):
