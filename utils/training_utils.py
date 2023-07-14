219bdde530632e38c0b08af85695a279dc746eda
Soumya Banerjee <soumyabanerjee@hotmail.com>
2023-07-13 23:43:24 -0400

    Added Knoledge Distilation

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -3,6 +3,8 @@ from typing import Dict, List
 import numpy as np
 import flwr as fl
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from tqdm import tqdm
 import wandb
 import pdb,traceback
@@ -86,6 +88,24 @@ def set_parameters(net, parameters: List[np.ndarray]):
     params_dict = zip(net.state_dict().keys(), parameters)
     state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
     net.load_state_dict(state_dict, strict=True)
+
+def loss_fn_kd(outputs, labels, teacher_outputs, params):
+    """
+    Compute the knowledge-distillation (KD) loss given outputs, labels.
+    "Hyperparameters": temperature and alpha
+
+    NOTE: the KL Divergence for PyTorch comparing the softmaxs of teacher
+    and student expects the input tensor to be log probabilities! See Issue #2
+    """
+    alpha = params.alpha
+    T = params.temperature
+    KD_loss = nn.KLDivLoss()(F.log_softmax(outputs/T, dim=1),
+                             F.softmax(teacher_outputs/T, dim=1)) * (alpha * T * T) + \
+              F.cross_entropy(outputs, labels) * (1. - alpha)
+
+    return KD_loss
+
+
 
 def train_single_epoch(net, trainloader, optimizer = None, criterion = None, DEVICE = get_device()):
     """Train the network on the training set."""
