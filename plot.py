ede6a971ee6b9175f2436e5819978e0e67e7e3ba
Soumya Banerjee <soumyabanerjee@hotmail.com>
2023-11-10 18:36:48 -0500

    revert

--- plot.py
+++ plot.py
@@ -9,13 +9,18 @@ file_prefix = "results/strategy_cifar10/eval_results_"
 
 
 
-def count_occurrences(data):
+def count_occurrences(data, c):
     # Count the occurrences of each value
     value_counts = Counter(data)
 
     # Extract values and counts for the bar plot
     values = list(value_counts.keys())
     counts = list(value_counts.values())
+    # print(values)
+    # print(counts)
+    print('printing...')
+    # pdb.set_trace()
+    plt.bar(values , counts,  alpha=0.5, label=f'round ', color=c)
     return values, counts
 
 def read_data(file_path):
@@ -27,6 +32,8 @@ def read_data(file_path):
     # data = []
     values = []
     counts = []
+    colours = ["tab:blue", "tab:red", "tab:green", "tab:purple"]
+    i=0
     for line in lines:
         data_i = []        
         data_i.extend(line.strip().split(','))
@@ -35,10 +42,12 @@ def read_data(file_path):
 
         # Convert the floating-point numbers to integers for counting
         data_i = [int(value) for value in data_i]
-        values_i, counts_i = count_occurrences(data_i)
-        values.extend(values_i)
-        counts.extend(counts_i) 
-        pdb.set_trace()
+        values_i, counts_i = count_occurrences(data_i, colours[i])
+        i+=1        
+        values.append(values_i)
+        counts.append(counts_i)  
+        # pdb.set_trace()    
+        # print(values, counts)   
     return values, counts
 
 
@@ -52,8 +61,8 @@ for i in range(min_val,max_val+1):
     file_path = file_prefix+ f"{i}.csv"
     try:
         values, counts = read_data(file_path)   
-        plt.bar(np.array(values[0]) + i * 0.02, counts[0], width=0.02, align='center', alpha=0.5, label=f'round {i} test', color="tab:red")
-        plt.bar(np.array(values[1]) + i * 0.02, counts[1], width=0.02, align='center', alpha=0.5, label=f'round {i} train', color="tab:blue")
+        # plt.bar(np.array(values[0]) + i * 0.02, counts[0], width=0.02, align='center', alpha=0.5, label=f'round {i} test', color="tab:red")
+        # plt.bar(np.array(values[1]) + i * 0.02, counts[1], width=0.02, align='center', alpha=0.5, label=f'round {i} train', color="tab:blue")
     except FileNotFoundError:
         continue
 
