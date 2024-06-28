72aafd1dba2adc54748e39c0797b80f175733036
soumyaxyz <soumyabanerjee@hotmail.com>
2024-06-28 19:30:55 -0400

    remapping fix

--- code/utilities/lib.py
+++ code/utilities/lib.py
@@ -19,6 +19,19 @@ model_name = ['efficientnet']
 model_train_mode = [0,2,3,5,10]
 combined_class = [True, False]
 
+def get_remapping(choice ='ABCD' ):
+    if choice == 'ABCD':
+        remapping = [[0,1,2,3,4], [5,6,7,8,9] , [10,11,12,13,14], [15,6,17,18,19]]
+    elif choice == 'BACD':
+        remapping = [ [5,6,7,8,9] ,[0,1,2,3,4],  [10,11,12,13,14], [15,6,17,18,19]]
+    elif choice == 'CABD':
+        remapping = [[10,11,12,13,14],  [0,1,2,3,4], [5,6,7,8,9] , [15,6,17,18,19]]
+    elif ...
+
+
+    
+    return remapping
+
 
 def modify_output(mode, target, function, *args, **kwargs):
     if mode =="all":
