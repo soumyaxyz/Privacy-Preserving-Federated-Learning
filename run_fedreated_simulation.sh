982c12bcc251e3dcff441833f7e5c8fae7ff9c2b
soumyaxyz <soumyabanerjee@hotmail.com>
2024-06-29 12:34:55 -0400

    fix

--- run_fedreated_simulation.sh
+++ run_fedreated_simulation.sh
@@ -11,7 +11,6 @@ base_dataset_name="CIFAR100"
 num_rounds=10
 num_clients=2
 threads=2
-root="saved_models/"
 weight=""
 split_count=1  # Default value for the single of iterations
 
@@ -50,17 +49,17 @@ do
             weight_flag="-w $weight"
        fi
    else
-       prev_file="local_model_$((i-1))"
+       prev_file="FL_global_model_$((i-1))"
        weight_flag="-w $prev_file"
    fi
 
    # Run the simulation script
    echo "Running create_and_run_simulation.sh -m $model_name -d $dataset_name -n $num_clients -r $num_rounds -t $threads $weight_flag"
-   /home/sbane002/Flare/Privacy-Preserving-Federated-Learning/create_and_run_simulation.sh -m $model_name -d $dataset_name -n $num_clients -r $num_rounds -t $threads $weight_flag
+   ./create_and_run_simulation.sh -m $model_name -d $dataset_name -n $num_clients -r $num_rounds -t $threads $weight_flag
 
    # Copy the file to the destination
    echo "Copying trained model weights to saved_models"
-   cp ./workspace/simulate_jobs/models/local_models/local_model.pt ./saved_models/local_model_$i.pt
+   cp ./workspace/simulate_jobs/app_server/local_models/FL_global_model.pt ./saved_models/FL_global_model_$i.pt
 
 #    # Rename the output file for the next iteration
 #    mv output_filename "filename_$i"
